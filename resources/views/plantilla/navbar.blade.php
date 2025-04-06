b57c78cf7a3ef214e335aa5b33d3bf291f65a23d
beto <albertocaballero1197@gmail.com>
2025-04-06 17:43:45 -0500

    Corrijo vista de reloj y edad del paciente en consulta y paciente

--- resources/views/plantilla/navbar.blade.php
+++ resources/views/plantilla/navbar.blade.php
@@ -45,7 +45,6 @@
                 const horaFormateada = ahora.toLocaleTimeString('es-ES', {
                     hour: '2-digit',
                     minute: '2-digit',
-                    second: '2-digit',
                     hour12: false
                 });
 
